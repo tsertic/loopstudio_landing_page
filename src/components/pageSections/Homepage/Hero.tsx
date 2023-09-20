afa31302ee0c61ac21289f371a64f86d8902c77c
tsertic <tsertic5@gmail.com>
2023-09-20 12:17:35 +0200

    updated paddings in hero section and changed hero image to darker one

--- src/components/pageSections/Homepage/Hero.tsx
+++ src/components/pageSections/Homepage/Hero.tsx
@@ -14,7 +14,7 @@ export const Hero = () => {
       </div>
       <div className="max-md:hidden absolute top-0 left-0 bottom-0 right-0 z-[-1]">
         <Image
-          src="/images/desktop/image-hero.jpg"
+          src="/images/desktop/image-hero.png"
           width={1440}
           height={650}
           alt="hero image"
@@ -22,7 +22,7 @@ export const Hero = () => {
         />
       </div>
       <div className="container-wide">
-        <div className="p-[24px] md:p-[40px] border-[2px] border-white text-white w-full md:max-w-[650px] ">
+        <div className="p-[24px] md:p-[40px] md:pb-[35px] border-[2px] border-white text-white w-full md:max-w-[650px] ">
           <h1 className=" text-headingXLMobile md:text-headingXL uppercase  ">
             Immersive
             <br />
